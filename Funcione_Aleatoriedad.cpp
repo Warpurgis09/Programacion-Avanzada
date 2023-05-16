f4d443fcd97e2f2f359ab430f7f9c460a88229ac
Warpurgis09 <124789404+Warpurgis09@users.noreply.github.com>
2023-05-16 10:40:02 -0500

    Add files via upload

---
+++ Funcione_Aleatoriedad.cpp
@@ -0,0 +1,27 @@
+/* 
+Fecha: 15/02/2023
+Autor: Rafael Torregroza
+Thema: Funciones, Aleatoriedad, Arreglos
+
+Problema: Crear uan funcion que genere numeros aleatorios  y graficar su frecuencia y tipo de histograma*/
+
+
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+cout<< "Bienbenido a el programa\nSelecione la opcion deseada" << std::endl;
+
+  int opcion, tamaño; 
+
+cout<<"\n1 Generar un conjunto de valores aleatorios \n2 Generar un histograma de los valors aleatorios \n3 Salir de la aplicacion"<< std::endl;
+  cin>> opcion;
+switch (opcion){
+  case 1: cout<< "\nIngrese que tantos numeros desea generar:";
+  
+  }
+}
---
+++ Tipos_Funciones.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/* 
+  Autor: Rafael Eduardo Torregroza Vidal
+  Fecha: 08/02/2023
+  Tema: Tipos de variables 
+
+
+
+
+*/
+
+
+
+
+
+
+
+int main(void) {
+
+printf("\n****************************************");
+printf("\n****************************************");
+printf("\n*****Diferentes tipos de variables******");
+printf("\n****************************************");
+printf("\n****************************************");
+printf("")
+
+
+  char = 1 Bytes: hola
+  short = 2 Bytes: -15
+  int = 2 Bytes: 1024
+  unsigned int = 2 Bytes: 685421//Solo numeros mayores a 0
+  long = 4 Bytes: 6416532465341
+  float = 4 Bytes: 10.2566
+  double = 8 Bytes: 0.0654
+  long double = 8 Bytes: 0.65416541653416354216354
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+   return 0;
+}
